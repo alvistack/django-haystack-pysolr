a2a9d17ce2fe3f5715962f4a2851b3371b5c9129
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-15 23:25:42 +0800

    alvistack/v3.11.0
        git clean -xdf
        tar zcvf ../python-pysolr_3.11.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-pysolr.spec ../python-pysolr_3.11.0-1.spec
        cp ../python*-pysolr*3.11.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/django-haystack-pysolr-3.11.0/
        rm -rf ../python*-pysolr*3.11.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="pysolr",
-    use_scm_version=True,
+    version="3.11.0",
     description="Lightweight Python client for Apache Solr",
     author="Daniel Lindsley",
     author_email="daniel@toastdriven.com",
@@ -26,5 +26,4 @@ setup(
     install_requires=["requests>=2.32.5", "setuptools"],
     python_requires=">=3.10",
     extras_require={"solrcloud": ["kazoo>=2.5.0"]},
-    setup_requires=["setuptools_scm"],
 )
